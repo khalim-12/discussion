bd45440b13b1c9544ac5bc2c4ea0ca2729341df7
Khalim Ssuna <khalimssuna12@gmail.com>
2023-10-07 15:31:57 +0600

    second push modified

--- string.js
+++ string.js
@@ -1,2 +1,11 @@
-let str = "welcome";
-console.log(str);
+// let str = "welcome";
+// console.log(str);
+//given the string s ,find the length of the longest substring without repeating the character
+//e.g s = "a,b,c,b,c,b,b,c" ans  = "a,b,c"
+//given two sorted rrays num1 and num2 of size m and n respectively,return the medium of the two sorted arrays.the overall run time complexity should be O(log(m+n))
+//example input : num1 = [1,3] num 2 = [2] output:2.0000
+//exp2 num1=[1,2] num2 =[3,4] output = 2.5000
+
+let s = "a,b,c,b,c,b,b,c";
+let str = s.split("").test().join("");
+alert(str);
